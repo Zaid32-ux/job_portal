5ffce9d8bcfd8d614d8e458feb7e74a0945031d5
Zaid32-ux <Zgour5071@gmail.com>
2025-12-05 17:21:19 +0530

    Add CORS to  support for cross-domain requests

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,20 @@ import userRouter from "./routes/userRoutes.js";
 import jobRouter from "./routes/jobRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
 import fileUpload from "express-fileupload";
+import cors from "cors";
 
 config({ path: "./.env" });//calling again in main file bcz load it globally its recommended
 const app = express();
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   fileUpload({

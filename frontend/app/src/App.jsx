8955397a85c4e2fb57e03539eb69d9aca5ebac3e
Zaid32-ux <Zgour5071@gmail.com>
2025-12-09 16:37:00 +0530

    add Navbar, Footer, Toaster and  Components to app layout

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,6 +1,20 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "./main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { Toaster } from "react-hot-toast";
 import axios from "axios";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Job/Jobs";
+import JobDetails from "./components/Job/JobDetails";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplications";
+import PostJob from "./components/Job/PostJob";
+import NotFound from "./components/NotFound/NotFound";
+import MyJobs from "./components/Job/MyJobs";
 
 function App() {
   const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
@@ -24,7 +38,23 @@ function App() {
 
   return (
     <>
-    <h1 className='bg-amber-50 text-4xl' >Job_portal</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/applications/me" element={<MyApplications />} />
+          <Route path="/job/post" element={<PostJob />} />
+          <Route path="/job/me" element={<MyJobs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </BrowserRouter>
     </>
   )
 }

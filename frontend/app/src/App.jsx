57a7fe797e4faadfdc5bf3cf25b0cac3dcedcff5
Zaid32-ux <Zgour5071@gmail.com>
2025-12-09 16:15:11 +0530

    Added user auth context and fetchUser logic in App.jsx

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
-
+import React, { useContext, useEffect } from "react";
+import { Context } from "./main";
+import axios from "axios";
 
 function App() {
- 
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthorized]); 
 
   return (
     <>

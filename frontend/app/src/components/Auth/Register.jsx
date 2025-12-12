51a50b1d32b145bd8ffca597c2f50e3e254cbb4c
Zaid32-ux <Zgour5071@gmail.com>
2025-12-12 14:27:18 +0530

    add register component logic, state management, and initial UI structure

---
+++ frontend/app/src/components/Auth/Register.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/v1/user/register",
+        { name, phone, email, role, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setName("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+      setRole("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthorized) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/careerconnect-black.png" alt="logo" />
+            <h3>Create a new account</h3>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;

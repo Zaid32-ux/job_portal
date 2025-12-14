ff0351f8090d902b0f2628deb3bd3be3ba8e72af
Zaid32-ux <Zgour5071@gmail.com>
2025-12-14 23:10:46 +0530

    add icons to hero section statistics cards

--- frontend/app/src/components/Home/HeroSection.jsx
+++ frontend/app/src/components/Home/HeroSection.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
 const HeroSection = () => {
   const details = [
     {
       id: 1,
       title: "1,23,441",
       subTitle: "Live Job",
-
+      icon: <FaSuitcase />,
     },
     {
       id: 2,
       title: "91220",
       subTitle: "Companies",
-    
+        icon: <FaBuilding />,
     },
     {
       id: 3,
       title: "2,34,200",
       subTitle: "Job Seekers",
+      icon: <FaUsers />,
     },
     {
       id: 4,
       title: "1,03,761",
       subTitle: "Employers",
+        icon: <FaUserPlus />,
     },
   ];
   return (

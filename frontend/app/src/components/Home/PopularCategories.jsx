3a4e6f375fa0c5d8b614619afa29fba7b1b61969
Zaid32-ux <Zgour5071@gmail.com>
2026-01-01 11:59:10 +0530

    add categories banner with cards

--- frontend/app/src/components/Home/PopularCategories.jsx
+++ frontend/app/src/components/Home/PopularCategories.jsx
@@ -64,6 +64,19 @@ const PopularCategories = () => {
   return (
     <div className="categories">
       <h3>POPULAR CATEGORIES</h3>
+      <div className="banner">
+        {categories.map((element) => {
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="text">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };

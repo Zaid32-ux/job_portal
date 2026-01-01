e3b4af943623a551386f6ca34ea4c91af9e409da
Zaid32-ux <Zgour5071@gmail.com>
2026-01-01 11:23:06 +0530

    add Top Companies banner component

--- frontend/app/src/components/Home/PopularCompanies.jsx
+++ frontend/app/src/components/Home/PopularCompanies.jsx
@@ -30,6 +30,22 @@ const PopularCompanies = () => {
     <div className="companies">
       <div className="container">
         <h3>TOP COMPANIES</h3>
+         <div className="banner">
+          {companies.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="content">
+                  <div className="icon">{element.icon}</div>
+                  <div className="text">
+                    <p>{element.title}</p>
+                    <p>{element.location}</p>
+                  </div>
+                </div>
+                <button>Open Positions {element.openPositions}</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );

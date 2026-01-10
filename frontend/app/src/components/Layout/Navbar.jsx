6ceb36c54d3011a10e46d54a509f63862f276712
Zaid32-ux <Zgour5071@gmail.com>
2026-01-10 14:18:31 +0530

    Add logout functionality with role-based navbar handling

--- frontend/app/src/components/Layout/Navbar.jsx
+++ frontend/app/src/components/Layout/Navbar.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Context } from "../../main";
+import { Link, useNavigate } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+     const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+     const navigateTo = useNavigate();
 
+     const handleLogout = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/api/v1/user/logout",
+        { withCredentials: true }
+      );
+      toast.success(res.data.message);
+      setIsAuthorized(false);
+      navigateTo("/login");
+    } catch (error) {
+      toast.error("Logout failed");
+    }
+  };
+
+  if (!isAuthorized) return null;
   return (
     <nav className="navbarShow">
       <div className="container">
@@ -24,9 +44,23 @@ const Navbar = () => {
           </li>
           <li>
             <Link to="/applications/me" onClick={() => setShow(false)}>
-              MY APPLICATIONS
+                {user?.role === "Employer"
+                ? "APPLICANT'S APPLICATIONS"
+                : "MY APPLICATIONS"}
             </Link>
           </li>
+                    {user?.role === "Employer" && (
+            <>
+              <li>
+                <Link to="/job/post">POST NEW JOB</Link>
+              </li>
+              <li>
+                <Link to="/job/me">VIEW YOUR JOBS</Link>
+              </li>
+            </>
+          )}
+
+          <button onClick={handleLogout}>LOGOUT</button>
         </ul>
 
         {/* Hamburger */}
